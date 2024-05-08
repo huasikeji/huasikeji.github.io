8ec09c059a40c3c065057262e9ca11f975deceda
攀哥 <www@yangzupan.com>
2024-05-09 05:16:34 +0800

    updata

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,30 +7,38 @@ function goToMain() {
 }
 // 夜间模式切换
 function switchDarkMode() {
+  function setThemeColor(color) {
+    document.querySelector('meta[name="theme-color"]').setAttribute('content', color)
+  }
   if (localStorage.getItem("color-theme")) {
     if (localStorage.getItem("color-theme") === "light") {
       document.documentElement.classList.add("dark");
-      document.querySelector('meta[name="theme-color"]').setAttribute('content', '#171717')
       localStorage.setItem("color-theme", "dark");
+      setThemeColor('#000000');
     } else {
       document.documentElement.classList.remove("dark");
       localStorage.setItem("color-theme", "light");
-      document.querySelector('meta[name="theme-color"]').setAttribute('content', '#ffffff')
-
+      setThemeColor('#ffffff');
     }
   } else {
     if (document.documentElement.classList.contains("dark")) {
       document.documentElement.classList.remove("dark");
       localStorage.setItem("color-theme", "light");
-      document.querySelector('meta[name="theme-color"]').setAttribute('content', '#ffffff')
-
+      setThemeColor('#ffffff');
     } else {
       document.documentElement.classList.add("dark");
       localStorage.setItem("color-theme", "dark");
-      document.querySelector('meta[name="theme-color"]').setAttribute('content', '#171717')
+      setThemeColor('#000000');
     }
   }
 }
+
+document.onkeydown=function(e){
+  if (e.ctrlKey && e.altKey){
+    switchDarkMode();
+  }
+}
+
 
 // 生成二维码
 var link = document.querySelectorAll("[data-qrcode-link]");
@@ -47,4 +55,23 @@ for (let i = 0; i < link.length; i++) {
     .catch((err) => {
       console.error(err);
     });
+}
+// 下载名片
+var download = document.querySelectorAll(".download");
+var card = document.querySelectorAll(".card");
+for (let i = 0; i < download.length; i++) {
+  download[i].addEventListener("click", function() {
+    html2canvas(card[i]).then((canvas) => {
+      canvas.toBlob((blob) => {
+        const href = window.URL.createObjectURL(new Blob([blob]));
+        const link2 = document.createElement("a");
+        const name = document.querySelectorAll(".name")[i].innerText;
+        link2.href = href;
+        link2.download = name + "\u7684\u540D\u7247.png";
+        document.body.appendChild(link2);
+        link2.click();
+        document.body.removeChild(link2);
+      }, "image/png");
+    });
+  }, false);
 }
